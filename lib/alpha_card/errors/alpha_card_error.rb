a0207d9b22c7a837e06a9c4233cff39ca100dd12
bulaj <bulajnikita@gmail.com>
2014-07-09 14:27:44 +0300

    documentation

--- lib/alpha_card/errors/alpha_card_error.rb
+++ lib/alpha_card/errors/alpha_card_error.rb
@@ -2,14 +2,20 @@ module AlphaCard
   ##
   # Common class for Alpha Card Gateway errors and exceptions.
   class AlphaCardError < StandardError
+    # Alpha Card Gateway response.
+    # @attr_reader [AlphaCard::AlphaCardResponse] response
     attr_reader :response
+    # Error message.
+    # @attr_reader [String] message
     attr_reader :message
 
     ##
     # <code>AlphaCard::AlphaCardError</code> constructor.
     #
-    # @param [String] error message
-    # @param [AlphaCard::AlphaCardResponse] AlphaCard Gateway response
+    # @param [String] response
+    #   Error message
+    # @param [AlphaCard::AlphaCardResponse] response
+    #   AlphaCard Gateway response
     #
     # @example
     #   AlphaCard::AlphaCardError.new
